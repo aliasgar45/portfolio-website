9b01e3d82679a43a1492d0176fae63719de0aa45
Aliasgar <aliasgardf@gmail.com>
2023-09-06 20:28:06 +0530

    Project Completed

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg:"#000000",
+    bgLight: "#000000",
+    primary:"#7CB9E8",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#000000",
+    card_light: '#191924',
+    button:"#7CB9E8",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#000000",
+    bgLight: "#000000",
+    primary:"#7CB9E8",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
+}
